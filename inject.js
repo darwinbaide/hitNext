23fbef4a98e38a94f66cc11ca81b44d596e54172
darwinbaide <dajers96@yahoo.com>
2020-07-22 12:36:31 -0400

    adding options

--- inject.js
+++ inject.js
@@ -22,3 +22,32 @@
     };
 
 })();
+
+
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+
+function setCookie() {
+    document.cookie = "username=John Doe; expires=Thu, 18 Dec 2029 12:00:00 UTC; path=/";
+}
+
+function getCookie(name) { //Gets the cookie
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    } else {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+            end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+}
